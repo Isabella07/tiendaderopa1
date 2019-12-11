9ebe45340ca9f13bf40c48ee87b840c940ebb651
Isabella07 <50912852+Isabella07@users.noreply.github.com>
2019-12-11 11:45:46 -0500

    Ropa

---
+++ tiendaderopa.sql
@@ -0,0 +1,13 @@
+-create table prenda(
+    codigo_de_prenda int, 
+    talla_de_prenda varchar (200),
+    color_de_prenda varchar (100),
+    ubicacion_d varchar (50)
+);
+
+insert into ropa (id,talla_de_prenda,color_de_prenda) values
+(1 "S","Rojo"),
+(2 "M", "Amarillo"),
+(3 "L", "Negro"),
+(4 "XL", "Gris"),
+(5 "XS", "Nude"),
